f88a1f8ec432c1cdb961f01191ce9372a1bfd6db
Subrat Gyawali <subrat.gyawali@gmail.com>
2022-03-04 07:12:52 +0545

    db connect updated with env

--- pkg/db/postgres/dbconnect.go
+++ pkg/db/postgres/dbconnect.go
@@ -12,16 +12,22 @@ import (
 // var DB *gorm.DB
 
 func ConnectDatabase() *gorm.DB {
-	dsn := fmt.Sprintf("host=localhost user=postgres password=password dbname=%s port=5432 sslmode=disable TimeZone=Asia/Shanghai", os.Getenv("DB_NAME"))
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=Asia/Shanghai",
+		os.Getenv("DB_HOST"), os.Getenv("DB_USERNAME"), os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_NAME"), os.Getenv("DB_PORT"),
+	)
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		panic(err)
 	}
-	db.AutoMigrate(
+	err = db.AutoMigrate(
 		&models.Contact{},
 		&models.Address{},
 		&models.Phone{},
 	)
+	if err != nil {
+		panic(err)
+	}
 	// DB = db
 	return db
 }
